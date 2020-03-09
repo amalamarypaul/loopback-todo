7ac12fd4b6244449b11a83b30779f9222730ae68
Amala Mary Paul <amalamarypaul@gmail.com>
2020-03-09 15:35:31 +0530

    Loopback 4- todo-list - simple todo in doc

---
+++ src/models/todo.model.ts
@@ -0,0 +1,37 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {}})
+export class Todo extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'string',
+  })
+  desc?: string;
+
+  @property({
+    type: 'boolean',
+  })
+  isComplete?: boolean;
+
+
+  constructor(data?: Partial<Todo>) {
+    super(data);
+  }
+}
+
+export interface TodoRelations {
+  // describe navigational properties here
+}
+
+export type TodoWithRelations = Todo & TodoRelations;
